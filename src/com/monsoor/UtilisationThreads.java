75fda99ec93b56c25f20a93f2eae463a17bb1a99
mmonsoo <misharl.monsoor@gmail.com>
2018-03-23 09:33:20 +0100

    second commit

---
+++ src/com/monsoor/UtilisationThreads.java
@@ -0,0 +1,12 @@
+package com.monsoor;
+
+public class UtilisationThreads {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Le nom du thread principal est " + Thread.currentThread().getName());
+
+	}
+
+}
